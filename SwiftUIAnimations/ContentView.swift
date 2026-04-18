e3c8e5f458ed56b5970194165c318e6bad03334e
Edwin Cardenas <eacardenase@gmail.com>
2026-04-18 18:43:24 -0500

    feat: animations on lifecycle events

--- SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/ContentView.swift
@@ -11,26 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
-        Button("Tap Me") {
-            guard animationAmount < 6 else {
-                animationAmount = 1
-
-                return
+        Button("Tap Me") {}
+            .padding(50)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .overlay {
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
             }
-
-            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-        .animation(
-            .easeInOut(duration: 1)
-            .repeatCount(3, autoreverses: true),
-            value: animationAmount
-        )
+            .onAppear {
+                animationAmount = 2
+            }
     }
 }
 
